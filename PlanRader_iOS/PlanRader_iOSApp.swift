71de50715205bddcda6105fa74099e68f7610bdd
Ahmed Azab Sanad <azab09701@gmail.com>
2025-12-13 01:18:45 +0200

    add swift project

--- PlanRader_iOS/PlanRader_iOSApp.swift
+++ PlanRader_iOS/PlanRader_iOSApp.swift
@@ -9,9 +9,12 @@ import SwiftUI
 
 @main
 struct PlanRader_iOSApp: App {
+    let persistenceController = PersistenceController.shared
+
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
         }
     }
 }
