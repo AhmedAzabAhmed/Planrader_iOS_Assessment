a51cc57b3b946254eed810f69b2301c49f1a05de
Ahmed Azab Sanad <azab09701@gmail.com>
2025-12-13 00:58:47 +0200

    add project

---
+++ PlanRader_iOS/PlanRader_iOS/PlanRader_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlanRader_iOSApp.swift
+//  PlanRader_iOS
+//
+//  Created by Ahmed Azzab Sanad on 13/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct PlanRader_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PlanRader_iOS/PlanRader_iOSTests/PlanRader_iOSTests.swift
@@ -0,0 +1,17 @@
+//
+//  PlanRader_iOSTests.swift
+//  PlanRader_iOSTests
+//
+//  Created by Ahmed Azzab Sanad on 13/12/2025.
+//
+
+import Testing
+@testable import PlanRader_iOS
+
+struct PlanRader_iOSTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
